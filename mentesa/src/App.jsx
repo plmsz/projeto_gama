486f2b59ed42a62f6af1dfb7ad76c6f14d6f66e7
paloma.souza <paloma.souza@ambevtech.com.br>
2022-08-29 14:53:35 -0300

    📦 build: criando aplicação react

---
+++ mentesa/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p className="read-the-docs">
+        Mente sã
+      </p>
+    </div>
+  )
+}
+
+export default App
