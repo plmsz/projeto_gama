f006feeb7a7ce5f55f1ad90c434eb743d2cd1533
paloma.souza <paloma.souza@ambevtech.com.br>
2022-08-29 15:55:39 -0300

    💄 feat: Estilizaçao global

---
+++ mentesa/src/styles/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components/'
+
+const GlobalStyle = createGlobalStyle`
+*{
+    font-family: Robot, sans-serif;
+}
+
+html {
+  font-size: 62.5%;
+}
+body{
+    margin: 0;
+    box-sizing: border-box;
+    padding: 0;
+    text-decoration: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+`
+export default GlobalStyle
