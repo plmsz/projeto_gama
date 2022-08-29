cf4e6a86801528f9f66bc755afb643438b89d83c
paloma.souza <paloma.souza@ambevtech.com.br>
2022-08-29 15:07:26 -0300

    📦 build: add react test library

---
+++ mentesa/src/App.test.jsx
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "vitest"
+import App from "./App";
+import { render, screen, userEvent } from "../test-utils";
+
+
+
+describe("Simple working test", () => {
+  it("the title is visible", () => {
+    render(<App />);
+    const welcomeText = screen.getByText(/Mente sã/i);
+    expect(welcomeText).toBeInTheDocument();
+
+  });
+})
