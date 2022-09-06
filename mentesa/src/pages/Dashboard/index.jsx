1e63524d7222ace41bd2cd047500d324470bc2a6
paloma.souza <paloma.souza@ambevtech.com.br>
2022-09-06 20:35:58 -0300

    fix - appointment list

--- mentesa/src/pages/Dashboard/index.jsx
+++ mentesa/src/pages/Dashboard/index.jsx
@@ -1,6 +1,5 @@
 import { useAuth } from '../../hooks/useAuth'
 import * as S from '../../styles/CommonUi'
-import { AppointmentList } from './components/AppointmentList/index'
 
 export function Dashboard() {
   const { user } = useAuth()
@@ -11,7 +10,6 @@ export function Dashboard() {
         <>
           <img src={user.avatar} alt={user.name} referrerPolicy='no-referrer' />
           <p>{user.name}</p>
-          <AppointmentList />
         </>
       )}
     </S.Container>
