adb5883a4a3af3f764dbe01d29ed65983bffbde1
paloma.souza <paloma.souza@ambevtech.com.br>
2022-09-06 18:42:59 -0300

    feat tabela

--- mentesa/src/pages/Dashboard/index.jsx
+++ mentesa/src/pages/Dashboard/index.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from '../../hooks/useAuth'
 import * as S from '../../styles/CommonUi'
+import { AppointmentList } from './components/AppointmentList/index'
 
 export function Dashboard() {
   const { user } = useAuth()
@@ -10,6 +11,7 @@ export function Dashboard() {
         <>
           <img src={user.avatar} alt={user.name} referrerPolicy='no-referrer' />
           <p>{user.name}</p>
+          <AppointmentList />
         </>
       )}
     </S.Container>
