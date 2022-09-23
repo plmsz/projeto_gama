647879eb4e7fc771649bb51b6ff5a79bcc1b6ab2
Tiago Silva <thyago29.05@gmail.com>
2022-09-23 18:02:02 -0300

    fix:adjustments-mobile

--- mentesa/src/Routes/Panel/styles.js
+++ mentesa/src/Routes/Panel/styles.js
@@ -22,4 +22,5 @@ export const Container = styled.div`
   box-shadow: 5px 2px 8px rgba(0, 0, 0, 0.14), 4px 5px 8px rgba(0, 0, 0, 0.14);
   border-radius: 4px;
   color: black;
+  overflow-x: scroll;
 `
